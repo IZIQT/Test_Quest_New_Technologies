a31cdc53384c181dc765e98d8d1e537499431a73
Евгений Третьяков <q78jk65@mail.ru>
2021-04-09 00:23:25 +0300

    Сделана выгрузка из файла

--- Test_Quest_New_Technologies/Model/MainWindowDataGridModel.cs
+++ Test_Quest_New_Technologies/Model/MainWindowDataGridModel.cs
@@ -14,7 +14,128 @@ namespace Test_Quest_New_Technologies.Model
             set
             {
                 loc_Date = value;
-                OnPropertyChanged(nameof(loc_Date));
+                OnPropertyChanged(nameof(Loc_Date));
+            }
+        }
+
+        private string object_A;
+        public string Object_A
+        {
+            get => object_A;
+            set
+            {
+                object_A = value;
+                OnPropertyChanged(nameof(Object_A));
+            }
+        }
+
+        private string type_A;
+        public string Type_A
+        {
+            get => type_A;
+            set
+            {
+                type_A = value;
+                OnPropertyChanged(nameof(Type_A));
+            }
+        }
+
+        private string object_B;
+        public string Object_B
+        {
+            get => object_B;
+            set
+            {
+                object_B = value;
+                OnPropertyChanged(nameof(Object_B));
+            }
+        }
+
+        private string type_B;
+        public string Type_B
+        {
+            get => type_B;
+            set
+            {
+                type_B = value;
+                OnPropertyChanged(nameof(Type_B));
+            }
+        }
+
+        private string direction;
+        public string Direction
+        {
+            get => direction;
+            set
+            {
+                direction = value;
+                OnPropertyChanged(nameof(Direction));
+            }
+        }
+
+        private string color;
+        public string Color
+        {
+            get => color;
+            set
+            {
+                color = value;
+                OnPropertyChanged(nameof(Color));
+            }
+        }
+
+        private string intensity;
+        public string Intensity
+        {
+            get => intensity;
+            set
+            {
+                intensity = value;
+                OnPropertyChanged(nameof(Intensity));
+            }
+        }
+
+        private double latitudeA;
+        public double LatitudeA
+        {
+            get => latitudeA;
+            set
+            {
+                latitudeA = value;
+                OnPropertyChanged(nameof(LatitudeA));
+            }
+        }
+
+        private double longitudeA;
+        public double LongitudeA
+        {
+            get => longitudeA;
+            set
+            {
+                longitudeA = value;
+                OnPropertyChanged(nameof(LongitudeA));
+            }
+        }
+
+        private double longitudeB;
+        public double LongitudeB
+        {
+            get => longitudeB;
+            set
+            {
+                longitudeB = value;
+                OnPropertyChanged(nameof(LongitudeB));
+            }
+        }
+
+        private double latitudeB;
+        public double LatitudeB
+        {
+            get => latitudeB;
+            set
+            {
+                latitudeB = value;
+                OnPropertyChanged(nameof(LatitudeB));
             }
         }
 
--- Test_Quest_New_Technologies/ViewModel/MainViewModel.cs
+++ Test_Quest_New_Technologies/ViewModel/MainViewModel.cs
@@ -1,7 +1,11 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.ObjectModel;
 using System.IO;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Input;
 using Test_Quest_New_Technologies.Common.MVVM;
 using Test_Quest_New_Technologies.Model;
@@ -34,20 +38,125 @@ namespace Test_Quest_New_Technologies.ViewModel
             }
         }
 
-        ICommand ExportCSVFile { get; set; }
+        public ICommand ExportCSVFile { get; set; }
 
         public MainViewModel()
         {
             ExportCSVFile = new RelayCommand(ExportCSVFileExecute);
         }
 
-        private void ExportCSVFileExecute(object obj)
+        private async void ExportCSVFileExecute(object obj)
         {
-            StringBuilder ExportText = new StringBuilder();
-            using (StreamReader sr = new StreamReader(selectedCSVFile))
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Csv файлы(*.csv)|*.csv|Все файлы (*.*)|*.*";
+            if (openFileDialog.ShowDialog() == true)
             {
-                ExportText = await sr.ReadToEndAsync();
+                try
+                {
+                    //using (StreamReader sr = new StreamReader(openFileDialog.FileName, Encoding.Default))
+                    //{
+                        MainWindowDataGrid = new ObservableCollection<MainWindowDataGridModel>();
+
+                        //await Task.Run(() =>
+                        //{
+                        //    for (int i = 0; i < 1000; i++)
+                        //    {
+                        //        MainWindowDataGrid.Add(
+                        //            new MainWindowDataGridModel()
+                        //            {
+                        //                Loc_Date = DateTime.Parse("02,02,2020"),
+                        //                Object_A = "02,02,2020",
+                        //                Type_A = "02,02,2020",
+                        //                Object_B = "02,02,2020",
+                        //                Type_B = "02,02,2020",
+                        //                Direction = "02,02,2020",
+                        //                Color = "02,02,2020",
+                        //                Intensity = "02,02,2020",
+                        //                LatitudeA = 60,
+                        //                LongitudeA = 60,
+                        //                LatitudeB = 60,
+                        //                LongitudeB = 60
+                        //            });
+                        //    }
+                        //});
+                        //string getLine;
+                        //while ((getLine = sr.ReadLine()) != null)
+                        //{
+                        //    string[] getSplitLine = getLine.Split(new[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
+                        //    DateTime tryToDate;
+                        //    if (getSplitLine.Length == 12 && DateTime.TryParse(getSplitLine[0], out tryToDate))
+                        //    {
+                        //        MainWindowDataGrid.Add(
+                        //            new MainWindowDataGridModel()
+                        //            {
+                        //                Loc_Date = DateTime.Parse(getSplitLine[0]),
+                        //                Object_A = getSplitLine[1],
+                        //                Type_A = getSplitLine[2],
+                        //                Object_B = getSplitLine[3],
+                        //                Type_B = getSplitLine[4],
+                        //                Direction = getSplitLine[5],
+                        //                Color = getSplitLine[6],
+                        //                Intensity = getSplitLine[7],
+                        //                LatitudeA = Convert.ToDouble(getSplitLine[8].Replace('.', ',')),
+                        //                LongitudeA = Convert.ToDouble(getSplitLine[9].Replace('.', ',')),
+                        //                LatitudeB = Convert.ToDouble(getSplitLine[10].Replace('.', ',')),
+                        //                LongitudeB = Convert.ToDouble(getSplitLine[11].Replace('.', ','))
+                        //            });
+                        //    }
+                        //}
+
+                        Task<ObservableCollection<MainWindowDataGridModel>> task1 = new Task<ObservableCollection<MainWindowDataGridModel>>(() =>
+                        {
+                            using (StreamReader sr = new StreamReader(openFileDialog.FileName, Encoding.Default))
+                            {
+                                ObservableCollection<MainWindowDataGridModel> localOC = new ObservableCollection<MainWindowDataGridModel>();
+                                string getLine;
+                                while ((getLine = sr.ReadLine()) != null)
+                                {
+                                    string[] getSplitLine = getLine.Split(new[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
+                                    DateTime tryToDate;
+                                    if (getSplitLine.Length == 12 && DateTime.TryParse(getSplitLine[0], out tryToDate))
+                                    {
+                                        localOC.Add(
+                                            new MainWindowDataGridModel()
+                                            {
+                                                Loc_Date = DateTime.Parse(getSplitLine[0]),
+                                                Object_A = getSplitLine[1],
+                                                Type_A = getSplitLine[2],
+                                                Object_B = getSplitLine[3],
+                                                Type_B = getSplitLine[4],
+                                                Direction = getSplitLine[5],
+                                                Color = getSplitLine[6],
+                                                Intensity = getSplitLine[7],
+                                                LatitudeA = Convert.ToDouble(getSplitLine[8].Replace('.', ',')),
+                                                LongitudeA = Convert.ToDouble(getSplitLine[9].Replace('.', ',')),
+                                                LatitudeB = Convert.ToDouble(getSplitLine[10].Replace('.', ',')),
+                                                LongitudeB = Convert.ToDouble(getSplitLine[11].Replace('.', ','))
+                                            });
+                                    }
+                                }
+                                return localOC;
+                            }
+                        });
+                    await Task.Run(() => { task1.Start(); }); 
+                    mainWindowDataGrid = task1.Result;
+                    //}
+                }
+                catch (System.IO.IOException)
+                {
+                    MessageBox.Show("Выбранный файл занят. Прожалуйста выбирете другой файл.", "Ошибка!");
+                }
+                catch (Exception exp)
+                {
+                    MessageBox.Show("Непредвиденная ошибка!\n]n" + exp, "Ошибка!");
+                }
             }
         }
+
+        private async void ReadFileExcute(string FileName)
+        {
+
+        }
+
     }
 }
