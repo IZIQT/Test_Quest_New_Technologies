5fe6c9595cbedace0c0e98af8526f9f15efa9843
Евгений Третьяков <q78jk65@mail.ru>
2021-04-08 19:53:04 +0300

    commit

---
+++ Test_Quest_New_Technologies/Model/MainWindowDataGridModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Test_Quest_New_Technologies.ViewModel;
+
+namespace Test_Quest_New_Technologies.Model
+{
+    class MainWindowDataGridModel:ViewModelBase
+    {
+        //        Date	Object A	Type A	Object B	Type B	Direction	Color	Intensity	LongitudeA	LongitudeA	LatitudeB	LongitudeB
+
+        private DateTime loc_Date;
+        public DateTime Loc_Date
+        {
+            get => loc_Date;
+            set
+            {
+                loc_Date = value;
+                OnPropertyChanged(nameof(loc_Date));
+            }
+        }
+
+    }
+}
---
+++ Test_Quest_New_Technologies/ViewModel/MainViewModel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Text;
+using System.Windows.Input;
+using Test_Quest_New_Technologies.Common.MVVM;
+using Test_Quest_New_Technologies.Model;
+
+namespace Test_Quest_New_Technologies.ViewModel
+{
+    class MainViewModel:ViewModelBase
+    {
+        private string selectedCSVFile;
+        public string SelectedCSVFile
+        {
+            get => selectedCSVFile;
+            set
+            {
+                if (selectedCSVFile == value) return;
+                selectedCSVFile = value;
+                OnPropertyChanged(nameof(SelectedCSVFile));
+            }
+        }
+
+        private ObservableCollection<MainWindowDataGridModel> mainWindowDataGrid;
+        public ObservableCollection<MainWindowDataGridModel> MainWindowDataGrid
+        {
+            get => mainWindowDataGrid;
+            set
+            {
+                if (mainWindowDataGrid == value) return;
+                mainWindowDataGrid = value;
+                OnPropertyChanged(nameof(MainWindowDataGrid));
+            }
+        }
+
+        ICommand ExportCSVFile { get; set; }
+
+        public MainViewModel()
+        {
+            ExportCSVFile = new RelayCommand(ExportCSVFileExecute);
+        }
+
+        private void ExportCSVFileExecute(object obj)
+        {
+            StringBuilder ExportText = new StringBuilder();
+            using (StreamReader sr = new StreamReader(selectedCSVFile))
+            {
+                ExportText = await sr.ReadToEndAsync();
+            }
+        }
+    }
+}
